0e93031d76e05fc07851c421ce6c65b411baa02e
“Eling <>
2021-05-25 20:58:27 -0700

    weather widget

--- lesson07/assignment/scripts.js
+++ lesson07/assignment/scripts.js
@@ -1,19 +1,19 @@
 
 	 window.weatherWidgetConfig =  window.weatherWidgetConfig || [];
 	 window.weatherWidgetConfig.push({
-			 selector:".weatherWidget",
+			 selector:".weatherForecastWidget",
 			 apiKey:"MBV3H2EP9GUJWZVWVCS6PY55F", //Sign up for your personal key
 			 location:"Seattle, WA, USA", //Enter an address
-			 unitGroup:"metric", //"us" or "metric"
-			 forecastDays:7, //how many days forecast to show
-			 title:"Seattle, USA", //optional title to show in the
-			 showTitle:true,
-			 showConditions:true
+			 unitGroup:"us", //"us" or "metric"
+			 // forecastDays:7, //how many days forecast to show
+			 // title:"Seattle, USA", //optional title to show in the
+			 // showTitle:true,
+			 // showConditions:true
 	 });
 
 	 (function() {
-	 var d = document, s = d.createElement('script');
-	 s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
-	 s.setAttribute('data-timestamp', +new Date());
-	 (d.head || d.body).appendChild(s);
-	 })();
+ 		var d = document, s = d.createElement('script');
+ 		s.src = 'https://www.visualcrossing.com/widgets/forecast/weather-forecast-widget-d3.js';
+ 		s.setAttribute('data-timestamp', +new Date());
+ 		(d.head || d.body).appendChild(s);
+ 		})();
